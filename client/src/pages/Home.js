3599ce28cb1f54dcc6d4b57cfe42e5c8d1dca969
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-23 19:03:46 -0400

    Updated Home page with the ThoughtList component to render thoughts from the graphql query.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import ThoughtList from "../components/ThoughtList";
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHTS } from "../utils/queries";
 
 const Home = () => {
+  // Use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  // Optional chaining negates the need to check if an object exists before accessing its properties
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">
+          {/* Loading becomes undefined when the query is complete,
+          allowing it to move on to the next part of the if/else */}
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ThoughtList
+              thoughts={thoughts}
+              title="Some Feed for Thought(s)..."
+            />
+          )}
+        </div>
       </div>
     </main>
   );
