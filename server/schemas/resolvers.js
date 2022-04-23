0fce35adbef9d97ff19a20cbd0a1fcb1fb9b6d56
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-22 22:13:58 -0400

    Added typeDefs and resolvers for single thought, all users, and single user.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,6 +10,24 @@ const resolvers = {
       /* Find thoughts, and sort in descending order of creation */
       return Thought.find(params).sort({ createdAt: -1 });
     },
+    // Get single thought by id
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // Get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // Get single user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
   },
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,6 +5,15 @@ const { gql } = require("apollo-server-express");
 // There are two passages to access APIs: queries and mutations
 // Queries = GET, Mutations = POST, PUT, DELETE
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
+
   type Thought {
     _id: ID
     thoughtText: String
@@ -22,7 +31,10 @@ const typeDefs = gql`
   }
 
   type Query {
+    users: [User]
+    user(username: String!): User
     thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
   }
 `;
 
@@ -31,3 +43,7 @@ module.exports = typeDefs;
 
 /* The (username: String) is a POSSIBLE parameter that can be used,
 but it is not required */
+
+// The ! after a query parameter means it must exist for the query to be carried out
+// Didn't do it for all thoughts or users because we simply return all of them
+// For a single thought or user, we must know which we are looking for, so it must exist
