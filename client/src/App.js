8a47f8adaefed52553edc16c75a683a6edd3348d
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-23 18:18:31 -0400

    Downgraded to React 17 for @apollo/client package, and enabled the Apollo Server for the app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-import Home from './pages/Home';
+import Home from "./pages/Home";
+// Link to the GraphQL server on the backend
+const httpLink = createHttpLink({
+  uri: "http://localhost:3001/graphql",
+});
+// Instantiate the Apollo Client instance to connect to the API endpoint
+const client = new ApolloClient({
+  link: httpLink,
+  // Instantiate a new cache object
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    // Enable entire app to interact with the Apollo Client instance
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
