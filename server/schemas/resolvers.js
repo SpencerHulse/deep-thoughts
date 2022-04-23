cef57a331ee3568bd4997dba4f1706ff17f3927a
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-22 20:50:55 -0400

    Created typeDefs and resolvers for fidning Thought data with Reactions.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,31 @@
+const { User, Thought } = require("../models");
+
 // Resolvers serve the response for typeDefs (Queries/Mutations)
 const resolvers = {
   Query: {
-    helloWorld: () => {
-      return "Hello world!";
+    /* Parent is more of a placeholder, allowing us to access the username second parameter */
+    thoughts: async (parent, { username }) => {
+      /* Const passes a username or empty object */
+      const params = username ? { username } : {};
+      /* Find thoughts, and sort in descending order of creation */
+      return Thought.find(params).sort({ createdAt: -1 });
     },
   },
 };
 
 module.exports = resolvers;
+
+/* resolvers can accept four arguments in the following order:
+
+parent - when using nested resolvers to handle complicated actions
+  holds reference to the resolver that executed the nested resolver function
+  not needed here, but it must come first anyway
+
+args - object of all values passed into a query or mutation request as params
+  above, username is destructured out
+
+context - used to make data accessible by all resolvers,
+  such as logged-in status or API access token
+
+info - extra info about operation's current state (not common)
+*/
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,10 +5,29 @@ const { gql } = require("apollo-server-express");
 // There are two passages to access APIs: queries and mutations
 // Queries = GET, Mutations = POST, PUT, DELETE
 const typeDefs = gql`
+  type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+    reactions: [Reaction]
+  }
+
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+
   type Query {
-    helloWorld: String
+    thoughts(username: String): [Thought]
   }
 `;
 
 // Export
 module.exports = typeDefs;
+
+/* The (username: String) is a POSSIBLE parameter that can be used,
+but it is not required */
