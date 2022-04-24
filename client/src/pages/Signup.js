d08e69bd8aa27af0c5fd5a8a513394d1cbd4d93a
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-24 16:49:47 -0400

    Added state to store form data for logging in and signing up, then added submit handling.

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
 
 const Signup = () => {
-  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  /* Initial state is blank, and it can also be updated upon submission
+  to clear the form data */
+  const [formState, setFormState] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  // Closure that creates and prepares a JS function around the mutation code, then returns it
+  // Gives the ability to check for errors as well
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,49 +24,62 @@ const Signup = () => {
     });
   };
 
-  // submit form
+  // submit form (async)
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    // try/catch instead of promises
+    try {
+      // execute addUser mutation and pass in variable data from the form
+      const { data } = await addUser({
+        // ... allows for any number of variables to be passed in and destructured
+        variables: { ...formState },
+      });
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
-    <main className='flex-row justify-center mb-4'>
-      <div className='col-12 col-md-6'>
-        <div className='card'>
-          <h4 className='card-header'>Sign Up</h4>
-          <div className='card-body'>
+    <main className="flex-row justify-center mb-4">
+      <div className="col-12 col-md-6">
+        <div className="card">
+          <h4 className="card-header">Sign Up</h4>
+          <div className="card-body">
             <form onSubmit={handleFormSubmit}>
               <input
-                className='form-input'
-                placeholder='Your username'
-                name='username'
-                type='username'
-                id='username'
+                className="form-input"
+                placeholder="Your username"
+                name="username"
+                type="username"
+                id="username"
                 value={formState.username}
                 onChange={handleChange}
               />
               <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                id="email"
                 value={formState.email}
                 onChange={handleChange}
               />
               <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                id="password"
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className='btn d-block w-100' type='submit'>
+              <button className="btn d-block w-100" type="submit">
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
