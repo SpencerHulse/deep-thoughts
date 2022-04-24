4dbf9d08020fd6d71cff9744afafa96fb1a2db0a
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-24 00:57:10 -0400

    Added navigation links.

--- client/src/App.js
+++ client/src/App.js
@@ -41,8 +41,9 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-              <Route exact path="/profile" component={Profile} />
-              <Route exact path="/thought" component={SingleThought} />
+              {/* ? after username means it is optional */}
+              <Route exact path="/profile/:username?" component={Profile} />
+              <Route exact path="/thought/:id" component={SingleThought} />
 
               <Route component={NoMatch} />
             </Switch>
