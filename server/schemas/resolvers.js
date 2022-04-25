90b382af131012e94c3a98e2dad7762cafb48c19
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-24 23:17:11 -0400

    Added dynamic rendering to the profile page and appropriate redirects for pathing. Fixed me resolver issue.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -34,7 +34,7 @@ const resolvers = {
     me: async (parent, args, context) => {
       // Check for context.user (JWT)
       if (context.user) {
-        const userData = await User.findOne({})
+        const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
           .populate("thoughts")
           .populate("friends");
